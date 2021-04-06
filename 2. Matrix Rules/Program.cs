d43f49534fd72a4caa535a7e41959a26c4a594e4
balimka <78259073+balimka@users.noreply.github.com>
2021-04-06 12:39:03 +0300

    Add project files.

---
+++ 2. Matrix Rules/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace _2._Matrix_Rules
+{
+    //https://pastebin.com/u73vGCFK
+    /*The Matrix Rules
+    The Architect did his best to make the current iteration of the Matrix as unpredictable and chaotic as possible. If only he could change the fundamental rules that governed the Matrix he would get rid of the Oracle once and for all!. Unfortunately for him the rules of the Matrix could not be changed. Not by him, not by anyone. This immutability of the rules was ensuring that the predictions of the Oracle would always be correct.
+
+    The source code of the Matrix is a long sequence of 0s and 1s. These 0s and 1s are called cells, for short, and each cell can be either dead (0) or alive (1). To give you a better understanding the sequence 101 has 2 alive cells and 1 dead cell whereas the sequence 11010 has 3 alive cells and 2 dead. By applying the fundamental rules to a sequence of code the Oracle can calculate its next state and calibrate her predictions of the future.
+
+    The fundamental rules of the Matrix are: 1. Any dead cell with less than two live neighbours remains dead. 2. Any dead cell with two live neighbours becomes a live cell. 3. Any live cell with two dead neighbours dies, as if by starvation. 4. Any live cell with two live neighbours dies, as if by overpopulation. 5. Any live cell with exactly one live neighbour survives.
+
+        Your task is to help the Oracle by applying the rules to several code seqences.
+
+        Input
+        Read from the standard input:
+
+        There is one line of input, a string:
+        101
+        Output
+        Print to the standard output:
+
+        One line of output - the next state of the sequence.
+        111
+        Constraints
+        Each cell has exactly two neighbours - left and right.
+        The left neighbour of the first cell is the last cell in the sequence.
+        The right neighbour of the last cell is the first cell in the sequence.
+        3 <= sequence length <= 50
+        Sample tests
+        Input
+        101
+        Output
+        111
+        Input
+        0111
+        Output
+        1101
+        Input
+        1001
+        Output
+        1001*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+
+            if (input[0] == '0')
+            {
+                if (input[1] == '1' && input[input.Length - 1] == '1')
+                {
+                    result.Append('1');
+                }
+                else
+                {
+                    result.Append('0');
+                }
+            }
+            else if (input[0] == '1')
+            {
+                if (input[1] == '0' && input[input.Length - 1] == '0' || input[1] == '1' && input[input.Length - 1] == '1')
+                {
+                    result.Append('0');
+                }
+                else
+                {
+                    result.Append('1');
+                }
+            }
+
+
+            for (int i = 1; i < input.Length - 1; i++)
+            {
+                char rightcell = input[i + 1];
+                char leftcell = input[i - 1];
+                char currentcell = input[i];
+
+                if(currentcell == '0')
+                {
+                    if(rightcell == '0' && leftcell == '0')
+                    {
+                        result.Append('0');
+                    }
+                    else if (rightcell == '1' && leftcell == '1')
+                    {
+                        result.Append('1');
+                    }
+                    else
+                    {
+                        result.Append('0');
+                    }
+                }
+                else
+                {
+                    if (rightcell == '1' && leftcell == '1')
+                    {
+                        result.Append('0');
+                    }
+                    else if (rightcell == '0' && leftcell == '0')
+                    {
+                        result.Append('0');
+                    }
+                    else
+                    {
+                        result.Append('1');
+                    }
+
+                }
+            }
+
+            if (input[input.Length - 1] == '0')
+            {
+                char rightcell = input[0];
+                char leftcell = input[input.Length - 2];
+                char currentcell = input[input.Length - 1];
+
+                if (rightcell == '1' && leftcell == '1')
+                {
+                    result.Append('1');
+                }
+                else
+                {
+                    result.Append('0');
+                }
+            }
+            else 
+            {
+                char rightcell = input[0];
+                char leftcell = input[input.Length - 2];
+                char currentcell = input[input.Length - 1];
+
+                if (rightcell == '0' && leftcell == '0')
+                {
+                    result.Append('0');
+                }
+                else if (rightcell == '1' && leftcell == '1')
+                {
+                    result.Append('0');
+                }
+                else
+                {
+                    result.Append('1');
+                }
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ 3. Data Protector/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _3._Data_Protector
+{   //https://pastebin.com/us95RT4k
+    /*Data Protector
+    You are beginning to resent your job as a Data Protector and why wouldn't you - just copying sequences of symbols over and over again. Data Protection is a mundane but important business - tough job, but somebody has to do it. While applying protection to the last two sequences, you notice something you haven't thought of before - it's the same process every time, just the symbols are different! For example, when you have the sequences 1 2 3 4 and a b c d, and the instructions 1 2, applying protection would mean just swapping everything between the first and second index (both inclusive) - so the protected sequences would be 1 b c 4 and a 2 3 d. And that's just the first level of protection, if there is another one with instructions 2 3, what would you get is (applied to the already protected sequences) 1 b 3 d and a 2 c 4. You start seeing the pattern and realize that if you automate this tedious process you'll never have to repeat it ever again!
+    
+    There is one tricky part though - sometimes the instructions are reversed! With the sequences 1 2 3 4 5 and a b c d e and instructions 4 1 you need to swap the last ones (at index 4) and then jump to the front to finish with indices 0 and 1. You are a little confused at first but then you write down the results a b 3 4 e and 1 2 c d 5 and suddenly it all makes perfect sense!
+
+        Input
+        Exactly three lines:
+
+        first sequence (separated by a space)
+        second sequence (separated by a space)
+        instructions (pairs separated by commas) - S1 E1, S2 E2, ..., Sn En
+        Output
+        Exactly two lines:
+
+        first protected sequence (separated by a space)
+        second protected sequence (separated by a space)
+        Constraints
+        the two sequences are always the same length
+        there are no invalid indices
+            Sample tests
+        Input
+        1 2 3 4
+        a b c d
+        1 2, 2 3
+        Output
+        1 b 3 d
+        a 2 c 4
+        Input
+        1 2 3 4 5
+        a b c d e
+        4 1, 2 4, 2 3
+        Output
+        a b 3 4 5
+        1 2 c d e*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> firstSeq = Console.ReadLine().Split().ToList();
+            List<string> secondSeq = Console.ReadLine().Split().ToList();
+
+            List<string> swapOperations = Console.ReadLine().Split(',').Select(t => t.Trim()).ToList();
+
+
+            for (int i = 0; i < swapOperations.Count; i++)
+            {
+
+                double firstIndex = char.GetNumericValue(swapOperations[i][0]);
+                double secondIndex = char.GetNumericValue(swapOperations[i][2]);
+
+                List<string> firstResult = new List<string>();
+                List<string> secondResult = new List<string>();
+
+
+                if (firstIndex < secondIndex)
+                {
+                    for (int j = 0; j < firstIndex; j++)
+                    {
+                        firstResult.Add(firstSeq[j]);
+                        secondResult.Add(secondSeq[j]);
+                    }
+                    for (int y = (int)firstIndex; y <= secondIndex; y++)
+                    {
+                        secondResult.Add(firstSeq[y]);
+                        firstResult.Add(secondSeq[y]);
+                    }
+                    if (secondIndex != firstSeq.Count)
+                    {
+                        for (int k = (int)secondIndex + 1; k < firstSeq.Count; k++)
+                        {
+                            firstResult.Add(firstSeq[k]);
+                            secondResult.Add(secondSeq[k]);
+                        }
+                    }
+                }
+                else if (firstIndex > secondIndex)
+                {
+                    for (int y = 0; y <= secondIndex; y++)
+                    {
+                        secondResult.Add(firstSeq[y]);
+                        firstResult.Add(secondSeq[y]);
+                    }
+
+                    for (int l = (int)secondIndex + 1; l < firstIndex; l++)
+                    {
+                        firstResult.Add(firstSeq[l]);
+                        secondResult.Add(secondSeq[l]);
+                    }
+                    
+                    for (int j = (int)firstIndex; j < firstSeq.Count; j++)
+                    {
+                        secondResult.Add(firstSeq[j]);
+                        firstResult.Add(secondSeq[j]);
+                    }
+
+
+                }
+
+                firstSeq = firstResult;
+                secondSeq = secondResult;
+            }
+            Console.WriteLine(String.Join(" ", firstSeq));
+            Console.WriteLine(String.Join(" ", secondSeq));
+
+        }
+    }
+}
